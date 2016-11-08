b1a12429a3c50958e3467952c6ad34f0dab78ad9
sber <sebastian.berndt@gmail.com>
2016-11-08 12:37:19 +0100

    renameExecs no longer produces backups

--- renameExecs.sh
+++ renameExecs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i -e 's@EXEC@'"$JAVA_EXECUTABLE"'@g' tw-exact
-sed -i -e 's@EXEC@'"$JAVA_EXECUTABLE"'@g' tw-exact-parallel
-sed -i -e 's@EXEC@'"$JAVA_EXECUTABLE"'@g'  tw-heuristic
-sed -i -e 's@EXEC@'"$JAVA_EXECUTABLE"'@g'  tw-heuristic-parallel
+sed -i 's@EXEC@'"$JAVA_EXECUTABLE"'@g' tw-exact
+sed -i 's@EXEC@'"$JAVA_EXECUTABLE"'@g' tw-exact-parallel
+sed -i 's@EXEC@'"$JAVA_EXECUTABLE"'@g'  tw-heuristic
+sed -i 's@EXEC@'"$JAVA_EXECUTABLE"'@g'  tw-heuristic-parallel
