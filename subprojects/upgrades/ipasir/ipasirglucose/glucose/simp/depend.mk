14fdab605b0b300469ba192ab594a7d37e712df3
sber <sebastian.berndt@gmail.com>
2017-01-12 15:41:45 +0100

    added glucose as upgrade

---
+++ subprojects/upgrades/ipasir/ipasirglucose/glucose/simp/depend.mk
@@ -0,0 +1,41 @@
+/media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/Main.o /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/Main.or /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/Main.od /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/Main.op: /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/Main.cc \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../utils/System.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/IntTypes.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../utils/ParseUtils.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../utils/Options.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Vec.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/XAlloc.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/Dimacs.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/SolverTypes.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Alg.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Map.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Alloc.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../simp/SimpSolver.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Queue.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/Solver.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Heap.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/BoundedQueue.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/Constants.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Clone.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/SolverStats.h
+/media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/SimpSolver.o /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/SimpSolver.or /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/SimpSolver.od /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/SimpSolver.op: \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/SimpSolver.cc \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Sort.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Vec.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/IntTypes.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/XAlloc.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../simp/SimpSolver.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Queue.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/Solver.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Heap.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Alg.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../utils/Options.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../utils/ParseUtils.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/SolverTypes.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Map.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Alloc.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/BoundedQueue.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/Constants.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../mtl/Clone.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../core/SolverStats.h \
+ /media/sf_mac/Temp/ipasir/sat/glucose4/glucose-4/simp/../utils/System.h
---
+++ subprojects/upgrades/ipasir/ipasirglucose/glucose/sources/simp/depend.mk
@@ -0,0 +1,40 @@
+/Users/audemard/Sat/Src/syrup/simp/Main.o /Users/audemard/Sat/Src/syrup/simp/Main.or /Users/audemard/Sat/Src/syrup/simp/Main.od /Users/audemard/Sat/Src/syrup/simp/Main.op: /Users/audemard/Sat/Src/syrup/simp/Main.cc \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/System.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/IntTypes.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/ParseUtils.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/Options.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Vec.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/XAlloc.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Dimacs.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/SolverTypes.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Alg.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Map.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Alloc.h \
+  /Users/audemard/Sat/Src/syrup/simp/../simp/SimpSolver.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Queue.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Solver.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Heap.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/BoundedQueue.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Constants.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Clone.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/SolverStats.h
+/Users/audemard/Sat/Src/syrup/simp/SimpSolver.o /Users/audemard/Sat/Src/syrup/simp/SimpSolver.or /Users/audemard/Sat/Src/syrup/simp/SimpSolver.od /Users/audemard/Sat/Src/syrup/simp/SimpSolver.op: /Users/audemard/Sat/Src/syrup/simp/SimpSolver.cc \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Sort.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Vec.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/IntTypes.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/XAlloc.h \
+  /Users/audemard/Sat/Src/syrup/simp/../simp/SimpSolver.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Queue.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Solver.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Heap.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Alg.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/Options.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/ParseUtils.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/SolverTypes.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Map.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Alloc.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/BoundedQueue.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Constants.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Clone.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/SolverStats.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/System.h
