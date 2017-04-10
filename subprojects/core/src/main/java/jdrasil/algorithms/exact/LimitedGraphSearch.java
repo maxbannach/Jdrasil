2b64aa1057cb60d9ac9b4a9566ea36b7a9ad8de4
bannach <bannach@tcs.uni-luebeck.de>
2017-04-10 12:43:12 +0200

    extended CAR2 to LimitedGraphSearch

--- subprojects/core/src/main/java/jdrasil/algorithms/exact/LimitedGraphSearch.java
+++ subprojects/core/src/main/java/jdrasil/algorithms/exact/LimitedGraphSearch.java
@@ -25,19 +25,27 @@ import java.util.*;
 import java.util.logging.Logger;
 
 /**
- * The tree width of a graph can be characterized from game theoretic point of view with the game of cops and robber.
- * This class implements this game from "the point of view of cleared subgraphs", that is, we do not hash positions of cops,
- * but already cleared areas (in which we can "see" where cops have to stand).
- * The implementation is based on the description in "Nondeterministic Graph Searching: From Pathwidth to Treewidth"
- * by Fomin, Fraigniaud and Nisse.
+ * The tree width of a graph can be characterized from game theoretic point of view with the graph search game where
+ * a team of searchers tries to catch a fugitive, who has unlimited speed. Depending on the visibility of the fugitive, a winning strategy of
+ * the searchers corresponds to a path- or tree-decomposition of the graph.
+ * Fomin, Fraigniaud and Nisse described a mixed version of the game in "Nondeterministic Graph Searching: From Pathwidth to Treewidth".
+ * Here, the fugitive is invisible, but he searchers may reveal her from time to time. By fixing the number \(q\) of reveals
+ * that are performed by the searchers, the game covers path decompositions (\(q=0\)) and tree decompositions (\(q=\infty\)),
+ * and, in particular, the area between these two extrema. In the cited paper such decompositions are called q-branched
+ * tree decompositions and they have, loosely speaking, the property that on each path from a root to the leafs there are at
+ * most \(q\) nodes with more then one children, i.e., at most \(q\) branching nodes.
+ *
  *
  * @param <T> vertex type of the graph
  * @author Max Bannach
  */
-public class CopsAndRobber2<T extends Comparable<T>> implements TreeDecomposer<T> {
+public class LimitedGraphSearch<T extends Comparable<T>> implements TreeDecomposer<T> {
 
     /** Jdrasils Logger */
     private final static Logger LOG = Logger.getLogger(JdrasilLogger.getName());
+
+    /** a good approximation of infinity ... */
+    private final int INFINITY = Integer.MAX_VALUE/2;
 
     /** The graph we wish to decomposed as array of BitSets */
     private BitSetGraph<T> graph;
@@ -46,89 +54,106 @@ public class CopsAndRobber2<T extends Comparable<T>> implements TreeDecomposer<T
     private int n;
 
     /** Hash map used for dynamic programming over subgraphs. */
-    private Map<BitSet, Boolean> memory;
+    private Map<BitSet, Integer> label;
 
-    /** Winning strategy of the cops, used to extract the tree decomposition. */
+    /** Winning strategy of the searchers, used to extract the tree decomposition. */
     private Map<BitSet, List<BitSet>> strategy;
+
+    /** The number of reveals we wish to use (we will compute smallest k that can use no more reveals) */
+    private int reveals;
 
     /**
      * The constructor will initialize data structures and translate the given graph into a BitSetGraph.
      * @param graph
      */
-    public CopsAndRobber2(Graph<T> graph) {
-        this.graph = new BitSetGraph<T>(graph);
-        this.n = this.graph.getN();
-        this.memory = new HashMap<>();
-        this.strategy = new HashMap<>();
+    public LimitedGraphSearch(Graph<T> graph) {
+        this(graph, graph.getVertices().size()+1);
     }
 
     /**
-     * Tries to decompose the graph (clear it / catch the robber) from the configuration in which S is already secured.
-     * In particular, it is assumed that the cops stand on the interior border of S.
+     * The constructor will initialize data structures and translate the given graph into a BitSetGraph.
+     * The q is the number of reveals the searcher are allowed to berform in the q-limited graph search game, i.e.,
+     * a q-branched tree decomposition is computed.
+     * @param graph
+     * @param q
+     */
+    public LimitedGraphSearch(Graph<T> graph, int q) {
+        this.graph = new BitSetGraph<T>(graph);
+        this.n = this.graph.getN();
+        this.label = new HashMap<>();
+        this.strategy = new HashMap<>();
+        this.reveals = q;
+    }
+
+    /**
+     * Tries to decompose the graph from the configuration in which S is already cleaned.
+     * In particular, it is assumed that the searchers stand on the interior border of S.
      * @param S
      * @param k
      * @return
      */
-    private boolean decompose(BitSet S, int k) {
-        if (memory.containsKey(S)) return memory.get(S); // cashed
+    private int decompose(BitSet S, int k) {
+        if (label.containsKey(S)) return label.get(S); // label of S was already computed
         strategy.put(S, new LinkedList<>());
-        if (S.cardinality() == n) { // catched the robber / cleaned the graph / done
-            memory.put(S, true);
-            return true;
+        if (S.cardinality() == n) { // end configuration is labeled with 0
+            label.put(S, 0);
+            return 0;
         }
-        boolean result = false;
+        int value = INFINITY; // default label is (almost) infinity
 
-        // the next move depends on the number of free cops we have
+        // the next move depends on the number of free searchers we have
         BitSet delta = graph.interiorBorder(S);
         if (delta.cardinality() < k) {
-            // we have a free cop, make an existential step
+            // we have a free searcher, make an existential step
             for (int v = 0; v < n; v++) {
                 if (S.get(v)) continue; // already cleared
                 BitSet newS = (BitSet) S.clone();
                 newS.set(v);
-                result |= decompose(newS, k);
-                if (result) {
+                int tmp = decompose(newS, k);
+                if (tmp < value) {
+                    value = tmp;
+                    strategy.get(S).clear();
                     strategy.get(S).add(newS);
-                    break;
                 }
             }
         } else {
-            // we have no free cop, we have to make an universal step and use S as separator
+            // we have no free searcher, we have to make an universal step and use S as separator
             List<BitSet> components = graph.separate(S);
             if (components.size() > 1) { // if we have not more then one component we lost
-                result = true; // it has to work for all, so we invert the logic
+                int tmp = 0; // it has to work for all, so we invert the logic
                 for (BitSet component : components) {
-                    // "guess" the component the robber is in by marking everything else safe
+                    // reveal the component the searcher is in by marking everything else safe
                     BitSet mask = new BitSet();
                     mask.set(0,n);
                     mask.andNot(component);
-                    result &= decompose(mask, k);
+                    tmp = Math.max(tmp, decompose(mask, k));
                     strategy.get(S).add(mask);
-                    if (!result) break;
+                    if (tmp == INFINITY) break;
                 }
-                if (!result) strategy.get(S).clear();
+                tmp = tmp + 1;
+                if (tmp < value) value = tmp;
             }
         }
 
-        // done, store result in memory and return
-        memory.put(S, result);
-        if (result == false) strategy.put(S, null); // safe some space
-        return result;
+        // done, store result in label and return
+        label.put(S, value);
+        if (value == INFINITY) strategy.put(S, null); // safe some space
+        return value;
     }
 
     /**
-     * Initial call method for @see jdrasil.algorithms.exact.CopsAndRobber2#decompose(java.util.BitSet, int)
+     * Initial call method for @see jdrasil.algorithms.exact.LimitedGraphSearch#decompose(java.util.BitSet, int)
      * @param k
      * @return
      */
     private boolean decompose(int k) {
-        memory.clear();
+        label.clear();
         strategy.clear();
-        return decompose(new BitSet(), k);
+        return decompose(new BitSet(), k) <= reveals;
     }
 
     /**
-     * Extract a tree decomposition from a winning strategy of the cops.
+     * Extract a tree decomposition from a winning strategy of the searchers.
      * Should be called after a run of @see decompose that has returned true
      * @param S
      * @param td
@@ -160,10 +185,10 @@ public class CopsAndRobber2<T extends Comparable<T>> implements TreeDecomposer<T
 
         int k = 1;
         while (!decompose(k)) {
-            LOG.info("tree width >= " + k);
+            LOG.info("limited search number >= " + k);
             k++;
         }
-        LOG.info("tree width == " + k);
+        LOG.info("limited search number == " + k);
         TreeDecomposition<T> td = new TreeDecomposition<T>(graph.getGraph());
         extractTreeDecomposition(new BitSet(), td);
         return td;
