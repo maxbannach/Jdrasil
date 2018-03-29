5f52688fbb955052c80b2e2341a63a4abd5390b2
bannach <bannach@tcs.uni-luebeck.de>
2018-03-29 13:51:15 +0200

    Refactor FlattenTreeDEcomposition

--- subprojects/core/src/main/java/jdrasil/algorithms/postprocessing/FlattenTreeDecomposition.java
+++ subprojects/core/src/main/java/jdrasil/algorithms/postprocessing/FlattenTreeDecomposition.java
@@ -4,9 +4,7 @@ import jdrasil.graph.Bag;
 import jdrasil.graph.Graph;
 import jdrasil.graph.TreeDecomposition;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * A tree-decomposition may contain useless bags. For instance, two adjacent bags with the same content, or an empty bag.
@@ -35,41 +33,63 @@ public class FlattenTreeDecomposition<T extends Comparable<T>> extends Postproce
      * Contract adjacent bags with same content.
      */
     private void contractDuplicateBags() {
-        Graph<Bag<T>> tree = treeDecomposition.getTree();
         if (treeDecomposition.getNumberOfBags() == 0) { return; }
+        Graph<Bag<T>> tree = treeDecomposition.getTree();
 
-        Set<Bag<T>> visited=new HashSet<>();
-        Stack<Bag<T>> S = new Stack<>();
+        Queue<Bag<T>> queue = new LinkedList<>();
+        for (Bag<T> v : tree) queue.offer(v);
 
-        Bag<T> root = tree.iterator().next();
-        S.push(root);
-        visited.add(root);
-        while (!S.isEmpty()) {
-            Bag<T> parent = S.pop();
-            boolean modified=true;
-            while (modified) {
-                modified=false;
-                for (Bag<T> child : tree.getNeighborhood(parent)) {
-                    if (!visited.contains(child)) {
-                        // TODO contraction is inconsequent!
-                        if (child.vertices.equals(parent.vertices)
-                                || (parent.vertices.containsAll(child.vertices) && (tree.getNeighborhood(child).size()<=2)) ) {
-                            tree.contract(parent, child);
-                            modified=true;
-                            break;
-                        } else {
-                            S.push(child);
-                            visited.add(child);
-                        }
+        Set<Bag<T>> removed = new HashSet<>();
+        while (!queue.isEmpty()) {
+            Bag<T> v = queue.poll();
+            if (removed.contains(v)) continue;
+            boolean contracted = true;
+            while (contracted) {
+                contracted = false;
+                for (Bag<T> w : tree.getNeighborhood(v)) {
+                    if (v.vertices.containsAll(w.vertices)) {
+                        tree.contract(v, w);
+                        removed.add(w);
+                        contracted = true;
+                        break;
                     }
                 }
             }
         }
 
+
+//        Set<Bag<T>> visited=new HashSet<>();
+//        Stack<Bag<T>> S = new Stack<>();
+//
+//        Bag<T> root = tree.iterator().next();
+//        S.push(root);
+//        visited.add(root);
+//        while (!S.isEmpty()) {
+//            Bag<T> parent = S.pop();
+//            boolean modified=true;
+//            while (modified) {
+//                modified=false;
+//                for (Bag<T> child : tree.getNeighborhood(parent)) {
+//                    if (!visited.contains(child)) {
+//                        // TODO contraction is inconsequent!
+//                        if (child.vertices.equals(parent.vertices)
+//                                || (parent.vertices.containsAll(child.vertices) && (tree.getNeighborhood(child).size()<=2)) ) {
+//                            tree.contract(parent, child);
+//                            modified=true;
+//                            break;
+//                        } else {
+//                            S.push(child);
+//                            visited.add(child);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
         treeDecomposition.setNumberOfBags(tree.getCopyOfVertices().size());
-        int id=1;
+        int id = 1;
         for (Bag<T> v : tree) {
-            v.id=id++;
+            v.id = id++;
         }
     }
 
